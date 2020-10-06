46b236db3715d090c8f95dc6ea3d91dfbd213867
heba-aboulmagd <heba.aboulmagd@gmail.com>
2020-10-06 14:22:52 +0200

    correct adding groups

--- big-sql/bigsql_grantDBADM.sh
+++ big-sql/bigsql_grantDBADM.sh
@@ -1,4 +1,4 @@
-echo 'Granting db2 priviledges to all students ...'
+echo 'Granting db2 DBADM priviledges to all students ...'
 db2 connect to bigsql
 db2 grant DBADM on database to group db2users
 db2 connect reset
