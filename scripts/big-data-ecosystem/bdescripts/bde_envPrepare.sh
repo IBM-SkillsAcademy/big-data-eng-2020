bdc02cd05027b54bc7a2249d8c2f32a79158d1ba
heba-aboulmagd <heba.aboulmagd@gmail.com>
2020-10-27 02:05:56 +0200

    check CPU and memory size

--- scripts/big-data-ecosystem/bdescripts/bde_envPrepare.sh
+++ scripts/big-data-ecosystem/bdescripts/bde_envPrepare.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+numMem=$(dmidecode -t memory | grep  Size: | grep -v "No Module Installed" | awk '{sum+=$2}END{print sum}')
+if [ $numMem -lt 124000 ]; then
+echo "Error: Memory size is less than 124 GB"; exit;
+fi
+echo "Memory size is $numMem";
+noOfCpus=$(dmidecode -t processor | grep 'Socket Designation: CPU #' | wc -l)
+if [ $noOfCpus -lt 12 ]; then
+   echo "Error: The number of CPUs is less than 12"
+   exit
+fi
+echo "The number of CPUs is $noOfCpus"
+
 function pause(){
     read -s -n 1 -p "Press any key to continue . . ."
     echo ""
 }
+
 
 echo 'Creating student accounts ...'
 source createStudentAccounts.sh
