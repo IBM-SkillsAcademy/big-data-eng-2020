76e0b8facf1ea16a8b7d8425c78e9756435c1fa2
AdelNour <45731373+AdelNour@users.noreply.github.com>
2020-11-29 18:19:15 +0200

    Update bde_executeExs.sh

--- scripts/big-data-ecosystem/workloadScripts/bde_executeExs.sh
+++ scripts/big-data-ecosystem/workloadScripts/bde_executeExs.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-start_all=$(date +%s.%N)
+#start_all=$(date +%s.%N)
 
-if  (( $1 >= $2 ) || [ -z "$1" ] || [ -z "$2" ]);
-then echo "wrong parameters"; exit;
+if  (( $1 >= $2 ) || [ -z "$1" ] || [ -z "$2" ]); then
+    echo "wrong parameters"
+    exit
 fi
 
 #clear log files
-echo "" > /workloadScripts/ex4_time
-echo "" > /workloadScripts/ex5_time
+echo "" > /workloadScripts/Adel/ex4p1_time
+echo "" > /workloadScripts/Adel/ex4p3_time
+echo "" > /workloadScripts/Adel/ex5_time
 
 for (( n=$1; n<=$2; n++ ))
 do
     current_student=student`echo $n | awk '{ printf "%04i\n", $0 }'`
-    /bin/su -c "/workloadScripts/bde_ex3.sh $current_student" - $current_student &
+    /bin/su -c "/workloadScripts/Adel/bde_ex3.sh $current_student" - $current_student &
 done
 wait
 echo 'Execution for Exercise 3 ends for all students'
 
+start_all=$(date +%s.%N)
+
 for (( n=$1; n<=$2; n++ ))
 do
     current_student=student`echo $n | awk '{ printf "%04i\n", $0 }'`
-	/bin/su -c "/workloadScripts/bde_ex4.sh $current_student" - $current_student &
+    /bin/su -c "/workloadScripts/Adel/bde_ex4.sh $current_student" - $current_student &
+    #sleep 2
 done
 wait
 echo 'Execution for Exercise 4 ends for all students'
@@ -29,7 +34,8 @@ echo 'Execution for Exercise 4 ends for all students'
 for (( n=$1; n<=$2; n++ ))
 do
     current_student=student`echo $n | awk '{ printf "%04i\n", $0 }'`
-	/bin/su -c "/workloadScripts/bde_ex5.sh $current_student" - $current_student &
+    /bin/su -c "/workloadScripts/Adel/bde_ex5.sh $current_student" - $current_student &
+    #sleep 2
 done
 wait
 echo 'Execution for Exercise 5 ends for all students'
@@ -45,7 +51,7 @@ echo 'Execution for Exercise 6 ends for all students'
 for (( n=$1; n<=$2; n++ ))
 do
     current_student=student`echo $n | awk '{ printf "%04i\n", $0 }'`
-	/bin/bash /workloadScripts/bde_ex7.sh $current_student "$3$n" &
+    /bin/bash /workloadScripts/bde_ex7.sh $current_student "$3$n" &
 done
 wait
 echo 'Execution for Exercise 7 ends for all students'
@@ -53,5 +59,4 @@ echo 'Execution for Exercise 7 ends for all students'
 duration_all=$(echo "$(date +%s.%N) - $start_all" | bc)
 execution_time_all=`printf "%.2f seconds" $duration_all`
 
-echo "$1 - Ex4:Part3 Execution Time: $execution_time_all"
-
+echo "All Execution Time: $execution_time_all"
