0ff62780e5c67771a9af1e38768a8a00f3724ae2
AdelNour <45731373+AdelNour@users.noreply.github.com>
2020-11-29 18:16:04 +0200

    Update bde_ex3.sh

--- scripts/big-data-ecosystem/workloadScripts/bde_ex3.sh
+++ scripts/big-data-ecosystem/workloadScripts/bde_ex3.sh
@@ -2,19 +2,19 @@
 
 echo '\nStart executing Ex 3 for ' $1 ' - part 1'
 cd /usr/hdp
-ls -l
-ls -l 3*
-ls -l current
+ls -l 1> /dev/null
+ls -l 3* 1> /dev/null
+ls -l current 1> /dev/null
 cd /etc
-ls -l
+ls -l 1> /dev/null
 cd /etc/zookeeper/conf
-ls -l
+ls -l 1> /dev/null
 
 echo '\nStart executing Ex 3 for ' $1 ' - part 2'
 hdfs dfs -mkdir Gutenberg
 cd /labfiles
 hdfs dfs -put *.txt Gutenberg
-hdfs dfs -ls -R /user/$1
+hdfs dfs -ls -R /user/$1 1> /dev/null
 
 echo '\nStart executing Ex 3 for ' $1 ' - part 3'
-hdfs fsck /user/$1/Gutenberg
+hdfs fsck /user/$1/Gutenberg 1> /dev/null
