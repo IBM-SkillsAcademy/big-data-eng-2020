cb5ebe687da4d7fd234280a7e0f0d2feb802f7de
AdelNour <45731373+AdelNour@users.noreply.github.com>
2020-11-29 18:17:46 +0200

    Update bde_ex5.sh

--- scripts/big-data-ecosystem/workloadScripts/bde_ex5.sh
+++ scripts/big-data-ecosystem/workloadScripts/bde_ex5.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+r=$(( ($RANDOM % 300 ) +1 ))
+echo 'Sleeping ' ${r} 's before Ex 5 for ' $1 ' ...'
+sleep ${r}s
+
 start=$(date +%s.%N)
 echo 'Start executing Ex 5 for ' $1 ' - part 1'
-ls /labfiles/WordCount2/
+ls /labfiles/WordCount2/ 1> /dev/null
 cp /labfiles/WordCount2/WordCount2.java .
-hadoop classpath
+hadoop classpath 1> /dev/null
 javac -cp `hadoop classpath` WordCount2.java
 jar cf WC2.jar *.class
 rm *.class
 hdfs dfs -put /labfiles/WordCount2/patternsToSkip
 hadoop jar WC2.jar WordCount2 -D wordcount.case.sensitive=false Gutenberg/*.txt wc2out -skip patternsToSkip
-hdfs dfs -ls wc2out
-hdfs dfs -cat wc2out/part-r-00000
+hdfs dfs -ls wc2out 1> /dev/null
+hdfs dfs -cat wc2out/part-r-00000 1> /dev/null
 hdfs dfs -rm -R wc2out
 
 duration=$(echo "$(date +%s.%N) - $start" | bc)
 execution_time=`printf "%.2f seconds" $duration`
 
-echo "$1 - Ex5:Part1 Execution Time: $execution_time" >> /workloadScripts/ex5_time
+echo "$1 - Ex5:Part1 Execution Time: $execution_time" >> /workloadScripts/Adel/ex5_time
