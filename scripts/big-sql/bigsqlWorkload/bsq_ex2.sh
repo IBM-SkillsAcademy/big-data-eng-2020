e91bf8ed4da4cf303f29c0dfdcb07ed2f6f0782d
kholy <elkholy@gmail.com>
2020-12-14 11:13:07 +0200

    Complete Exercise 2 Alpha

--- scripts/big-sql/bigsqlWorkload/bsq_ex2.sh
+++ scripts/big-sql/bigsqlWorkload/bsq_ex2.sh
@@ -17,8 +17,19 @@ su $1 -c "hadoop fs -ls /user/$1/"
 su $1 -c "hadoop fs -copyFromLocal /usr/ibmpacks/bigsql/6.0.0.0/bigsql/samples/data/ /user/$1/sampledata"
 su $1 -c "hadoop fs -ls /user/$1/sampledata/data"
 
-echo 'Start executing Ex 2 for ' $1 ' - part 4'
+echo 'Start executing Ex 2 for ' $1 ' - part 4,5,6'
 
 
 su $1 -c "/usr/ibmpacks/common-utils/current/jsqsh/bin/jsqsh bigsql < /workloadScripts/bsq_ex2_B.sql -C $JSQSH_CONF -U $1 -P $2"
+
+echo 'Start executing Ex 2 for ' $1 ' - part 7'
+
+
+	
+	
+
+su $1 -c "hdfs dfs -mkdir /user/$1/bigsql_lab"
+su $1 -c "hdfs dfs -mkdir /user/$1/bigsql_lab/sls_product_dim"
+su $1 -c "hdfs dfs -chmod -R 777 /user/$1/bigsql_lab"
+
 
