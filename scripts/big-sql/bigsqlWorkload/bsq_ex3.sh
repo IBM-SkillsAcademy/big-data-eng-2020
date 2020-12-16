f5c0524ce0dc1de4bfaeedb1a899716b269dffff
kholy <elkholy@gmail.com>
2020-12-16 08:21:03 +0200

    troubleshooting error in ex2 (file doesnt exist)

--- scripts/big-sql/bigsqlWorkload/bsq_ex3.sh
+++ scripts/big-sql/bigsqlWorkload/bsq_ex3.sh
@@ -14,6 +14,7 @@ su $current_student -c "/usr/ibmpacks/common-utils/current/jsqsh/bin/jsqsh bigsq
 echo 'Start executing Ex 3 for ' $current_student ' - part 3'
 
 su $current_student -c "mkdir -p /home/$current_student/"
+su $current_student -c "hadoop fs -mkdir -p /home/$current_student/"
 su $current_student -c "/bin/cp /home/bigsql/labfiles/bigsql/BIG_SQL_Data_Analysis/Array_Data.txt /home/$current_student/product_line_array.csv"
 su $current_student -c "cat /home/$current_student/product_line_array.csv"
 
