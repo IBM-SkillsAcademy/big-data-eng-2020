0812a1b349dcfbce26b49ac2c53403110c70dcfe
kholy <elkholy@gmail.com>
2020-12-16 18:12:02 +0200

    minor updates

--- scripts/big-sql/bigsqlWorkload/bsq_ex6.sh
+++ scripts/big-sql/bigsqlWorkload/bsq_ex6.sh
@@ -9,6 +9,7 @@ DB2_USERNAME=$4
 DB2_PASSWORD=$5
 DB2_PORT=$6
 DB2_DB=$7
+BIGSQL_PASSWD=$8
 su $current_student -c "/usr/ibmpacks/common-utils/current/jsqsh/bin/jsqsh bigsql < /workloadScripts/bsq_ex2_A.sql  -U $current_student -P $current_student_bigsql_passwd"
 
 
@@ -20,17 +21,18 @@ echo 'Start executing Ex 6 for ' $current_student
 
 
 /bin/cp $GIT_REPO_DIR/scripts/big-sql/bigsqlWorkload/bsq_ex6_A.sql $SCRIPTS_PATH/bsq_ex6_A_$current_student.sql
-sed -i 's/db2_username/$DB2_USERNAME/' $SCRIPTS_PATH/bsq_ex6_A_$current_student.sql
-sed -i 's/db2_password/$DB2_PASSWORD/' $SCRIPTS_PATH/bsq_ex6_A_$current_student.sql
-sed -i 's/db2_hostname/$DB2_HOSTNAME/' $SCRIPTS_PATH/bsq_ex6_A_$current_student.sql
-sed -i 's/db2_port/$DB2_PORT/' $SCRIPTS_PATH/bsq_ex6_A_$current_student.sql
-sed -i 's/db2_name/$DB2_DB/' $SCRIPTS_PATH/bsq_ex6_A_$current_student.sql
-sed -i 's/username/$current_student/' $SCRIPTS_PATH/bsq_ex6_A_$current_student.sql
+/bin/cp $GIT_REPO_DIR/scripts/big-sql/bigsqlWorkload/bsq_ex6_B.sql $SCRIPTS_PATH/bsq_ex6_B_$current_student.sql
+
 su $current_student -c "/usr/ibmpacks/common-utils/current/jsqsh/bin/jsqsh bigsql < /workloadScripts/bsq_ex6_A_$current_student.sql  -U $current_student -P $current_student_bigsql_passwd"
 rm $SCRIPTS_PATH/bsq_ex6_A_$current_student.sql
 
-
-echo 'Start executing Ex 2 for ' $current_student ' - part 7'
+sed -i "s/db2_username/$DB2_USERNAME/" $SCRIPTS_PATH/bsq_ex6_B_$current_student.sql
+sed -i "s/db2_password/$DB2_PASSWORD/" $SCRIPTS_PATH/bsq_ex6_B_$current_student.sql
+sed -i "s/db2_hostname/$DB2_HOSTNAME/" $SCRIPTS_PATH/bsq_ex6_B_$current_student.sql
+sed -i "s/db2_port/$DB2_PORT/" $SCRIPTS_PATH/bsq_ex6_B_$current_student.sql
+sed -i "s/db2_name/$DB2_DB/" $SCRIPTS_PATH/bsq_ex6_B_$current_student.sql
+sed -i "s/username/$current_student/" $SCRIPTS_PATH/bsq_ex6_B_$current_student.sql
+#echo 'Start executing Ex 2 for ' $current_student ' - part 7'
 
 
 	
