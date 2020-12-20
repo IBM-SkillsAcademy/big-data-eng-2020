46888549854bf387b486bdc3159e94b3183f4d74
kholy <elkholy@gmail.com>
2020-12-20 17:21:08 +0200

    adding white spaces to sql code

--- scripts/big-sql/bigsqlWorkload/bsq_ex6_A.sql
+++ scripts/big-sql/bigsqlWorkload/bsq_ex6_A.sql
@@ -1,4 +1,4 @@
-if not exists(Select 1 From SysCat.Servers where ServerName='DB2_ON_CLOUD')
+if not exists ( Select 1 From SysCat.Servers where ServerName='DB2_ON_CLOUD' )
     create server db2_on_cloud type DB2/UDB version 11
     authorization db2_username password "db2_password" 
     options (host 'db2_hostname', port 'db2_port', dbname 'db2_name', password 'Y');
