aaac88417725cebfe79f9329ff7fbe38125f991a
kholy <elkholy@gmail.com>
2020-12-20 14:29:37 +0200

    minor fix for debugging

--- scripts/big-sql/bigsqlWorkload/bsq_ex6.sh
+++ scripts/big-sql/bigsqlWorkload/bsq_ex6.sh
@@ -16,7 +16,7 @@ BIGSQL_PASSWD=$8
 #su $current_student -c "/usr/ibmpacks/common-utils/current/jsqsh/bin/jsqsh bigsql < /home/bigsql/labfiles/bigsql/BIG_SQL_Data_Analysis/1_CREATE\ TABLE\ statements.sql"
 #su $current_student -c "/usr/ibmpacks/common-utils/current/jsqsh/bin/jsqsh bigsql < /bigsqlWorkload/bsq_ex2_part2.sql"
 
-echo 'Start executing Ex 6 for ' $current_student 
+echo 'Start executing Ex 6 for ' $current_student 'Part 1'
 
 
 
@@ -41,7 +41,9 @@ sed -i "s/db2_username/$DB2_USERNAME/" $SCRIPTS_PATH/bsq_ex6_B_$current_student.
 #sed -i "s/username/$current_student/" $SCRIPTS_PATH/bsq_ex6_B_$current_student.sql
 #echo 'Start executing Ex 2 for ' $current_student ' - part 7'
 
-su $current_student -c "/usr/ibmpacks/common-utils/current/jsqsh/bin/jsqsh bigsql < /workloadScripts/bsq_ex6_A_$current_student.sql  -U bigsql -P $current_student_bigsql_passwd"
+
+echo 'Start executing Ex 6 for ' $current_student 'Part 2'
+su $current_student -c "/usr/ibmpacks/common-utils/current/jsqsh/bin/jsqsh bigsql < /workloadScripts/bsq_ex6_B_$current_student.sql  -U bigsql -P $current_student_bigsql_passwd "
 #rm $SCRIPTS_PATH/bsq_ex6_A_$current_student.sql
 
 	
