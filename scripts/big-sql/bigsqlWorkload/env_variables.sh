860acb0e8d7aecd070548e12238b386f53da0556
kholy <elkholy@gmail.com>
2020-12-21 10:47:49 +0200

    add env_variables.sh file

---
+++ scripts/big-sql/bigsqlWorkload/env_variables.sh
@@ -0,0 +1,11 @@
+export DB2_HOSTNAME=xxxxx
+export DB2_USERNAME=xxxxx
+export DB2_PASSWORD=xxxxx
+export DB2_PORT=xxxxx
+export DB2_DB=xxxxx
+export BIGSQL_PASSWD=xxxxx
+
+#!/bin/bash
+export SCRIPTS_PATH=/workloadScripts
+export GIT_REPO_DIR=/root/bigsql/scriptsbigsql/big-data-eng-2020
+
