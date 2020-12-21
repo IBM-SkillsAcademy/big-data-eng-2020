ce6632402bcbe62037841b563e1ff1e86657c095
kholy <elkholy@gmail.com>
2020-12-21 16:28:25 +0200

    fix mistake of delete part of sql

--- scripts/big-sql/bigsqlWorkload/bsq_ex6_B.sql
+++ scripts/big-sql/bigsqlWorkload/bsq_ex6_B.sql
@@ -1,9 +1,4 @@
-/*
-
-create user mapping for username server db2_on_cloud options (REMOTE_AUTHID 'db2_username', REMOTE_PASSWORD 'db2_password');
-*/
-create nickname db2_on_cloud_gosalesdw_mrk_promotion_fact 
-for db2_on_cloud.db2_username.mrk_promotion_fact;
+create nickname db2_on_cloud_gosalesdw_mrk_promotion_fact for db2_on_cloud.db2_username.mrk_promotion_fact;
 
 select * from db2_on_cloud_gosalesdw_mrk_promotion_fact;
 
