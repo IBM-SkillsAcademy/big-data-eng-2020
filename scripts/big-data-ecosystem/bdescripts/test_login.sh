5f45e1f709a30996f0f22b5078c36717f04e7e5d
kholy <elkholy@gmail.com>
2021-01-05 14:21:49 +0200

    fixing errors in test_login.sh

--- scripts/big-data-ecosystem/bdescripts/test_login.sh
+++ scripts/big-data-ecosystem/bdescripts/test_login.sh
@@ -11,7 +11,7 @@ epassword=$(sudo getent shadow $USERNAME | cut -d: -f2)
 match=$(python -c 'import crypt; print crypt.crypt("'"${PASSWORD}"'", "$6$'${salt}'")')
 # s[ ${match} == ${epassword} ] && echo "$USERNAME Password matches" || echo "$USERNAME Password doesn't match"
 
-if s[ ${match} == ${epassword} ]
+if [ ${match} == ${epassword} ]
 then 
     echo "$USERNAME Password matches"
 else 
