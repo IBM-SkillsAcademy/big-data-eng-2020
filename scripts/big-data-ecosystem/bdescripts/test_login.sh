f5d8893f1d0dfee0e2303768d176c647f2ca0319
kholy <elkholy@gmail.com>
2021-01-05 14:22:51 +0200

    remove debug lines test_login.sh

--- scripts/big-data-ecosystem/bdescripts/test_login.sh
+++ scripts/big-data-ecosystem/bdescripts/test_login.sh
@@ -1,7 +1,7 @@
 export USERNAME=$1
 export PASSWORD=$2
-echo $USERNAME
-echo $PASSWORD
+#echo $USERNAME
+#echo $PASSWORD
 #echo  "$PASSWORD" | su -c whoami "$USERNAME"
 
 #read -p "Username >" username
