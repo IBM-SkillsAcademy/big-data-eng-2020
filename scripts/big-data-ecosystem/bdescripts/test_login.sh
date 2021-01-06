b8068dc6ad6d23b495e854d0d9562cbbd1d7620d
kholy <elkholy@gmail.com>
2021-01-06 09:42:42 +0200

    remove debug lines

--- scripts/big-data-ecosystem/bdescripts/test_login.sh
+++ scripts/big-data-ecosystem/bdescripts/test_login.sh
@@ -10,8 +10,8 @@ salt=$(sudo getent shadow $USERNAME | cut -d$ -f3)
 epassword=$(sudo getent shadow $USERNAME | cut -d: -f2)
 match=$(python -c 'import crypt; print crypt.crypt("'"${PASSWORD}"'", "$6$'${salt}'")')
 # s[ ${match} == ${epassword} ] && echo "$USERNAME Password matches" || echo "$USERNAME Password doesn't match"
-echo $match
-echo $epassword
+#echo $match
+#echo $epassword
 if [ ${match} == ${epassword} ]
 then 
     echo "$USERNAME Password matches"
