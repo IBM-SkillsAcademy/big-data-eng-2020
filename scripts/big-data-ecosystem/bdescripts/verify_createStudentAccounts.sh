6decbf17bb3084f9ab672c7a3d3053a18cf84b58
kholy <elkholy@gmail.com>
2021-01-06 16:45:53 +0200

    Update verify_createStudentAccounts.sh

--- scripts/big-data-ecosystem/bdescripts/verify_createStudentAccounts.sh
+++ scripts/big-data-ecosystem/bdescripts/verify_createStudentAccounts.sh
@@ -3,9 +3,10 @@ rm -rf success_create_student_A.txt
 rm -rf fail_create_student_A.txt 
 rm -rf students_dir.txt
 rm -rf test_create_student_C.txt 
+rm -rf created_users.txt
 
 export GROUPID=`grep students /etc/group | cut -f 3 -d :`
-echo $GROUPID
+#echo $GROUPID
 if [ -z "$GROUPID" ]
 then 
   echo "group students creation failed"
@@ -34,13 +35,27 @@ awk -v RS='\r?\n' ' BEGIN { FS = OFS = "," }
     }
 ' users.csv
 
+grep $GROUPID /etc/passwd | grep student | cut -f 1 -d ":" | sort | uniq > created_users.txt
+
+
 hadoop fs -ls -h /user/ | grep student > test_create_student_C.txt 
 cat test_create_student_C.txt  | grep hdfs | grep drwxr--r-x | cut -f 5 -d " " | sort | uniq > students_dir.txt 
-
+MD5_CREATED=(`md5sum created_users.txt`)
 MD5_DIR=(`md5sum students_dir.txt`)
 MD5_USERS=(`md5sum users_list.txt`) 
+
 if [ "$MD5_DIR" == "$MD5_USERS" ]; then
-  echo -e "\nuser directories created successfully"
+  echo -e "\nuser accounts created successfully"
+else 
+  echo -e "\nuser accounts failed to be created correctly\n"
+  read -s -n 1 -p "Press any key to exit"
+  exit 1
+fi
+
+
+
+if [ "$MD5_DIR" == "$MD5_USERS" ]; then
+  echo -e "\nuser directories created successfully\n"
 else 
   echo "user directories creation has problems, review list of directories and check their owners, groups and permissions for more details"
   cat test_create_student_C.txt | grep -v hdfs
