a57342223116f69b483ee06712a676089b5686ad
kholy <elkholy@gmail.com>
2021-01-18 11:12:44 +0200

    Update verify_genHbasescript.sh

--- scripts/big-data-ecosystem/bdescripts/verify_genHbasescript.sh
+++ scripts/big-data-ecosystem/bdescripts/verify_genHbasescript.sh
@@ -2,7 +2,7 @@ awk -v RS='\r?\n' '
     BEGIN { FS = OFS = "," } 
     { 
       system("./test_hbase.sh "$1);
-      system("su hbase -c '\''echo \"user_permission '\''\\'\'''\''ns_"$1"'\''\\'\'''\''\" | hbase shell -n '\''");
+      #system("su hbase -c '\''echo \"user_permission '\''\\'\'''\''ns_"$1"'\''\\'\'''\''\" | hbase shell -n '\''");
       #system("su hbase -c '\''echo \"grant '\''\\'\'''\''"$1"'\''\\'\'''\'', '\''\\'\'''\''RWXC'\''\\'\'''\'', '\''\\'\'''\''@ns_"$1"'\''\\'\'''\''\" | hbase shell -n '\''");
       
     }
