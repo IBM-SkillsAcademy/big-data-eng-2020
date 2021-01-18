adc716537e4248dddd111ca3ba3caef1477b8d88
kholy <elkholy@gmail.com>
2021-01-18 11:34:17 +0200

    Update verify_genHbasescript.sh

--- scripts/big-data-ecosystem/bdescripts/verify_genHbasescript.sh
+++ scripts/big-data-ecosystem/bdescripts/verify_genHbasescript.sh
@@ -1,3 +1,7 @@
+rm -rf hbase_files.txt
+rm -rf hbase_results.txt
+rm -rf users_list.txt
+
 awk -v RS='\r?\n' '
     BEGIN { FS = OFS = "," } 
     { 
@@ -7,6 +11,21 @@ awk -v RS='\r?\n' '
       
     }
 ' users.csv  > hbase_results.txt
+cat hbase_results.txt | grep Permission | grep READ,WRITE,EXEC,CREATE | sort | uniq |  cut -c 2-12 > hbase_files.txt
+
+cut -f 1 -d "," users.csv | sort | uniq > users_list.txt
+
+MD5_HBASE=(`md5sum hbase_files.txt`)
+MD5_USERS=(`md5sum users_list.txt`) 
+
+
+if [ "$MD5_HBASE" == "$MD5_USERS" ]; then
+  echo -e "\nHBase namespaces created successfully"
+else 
+  echo -e "\nHBase namespaces failed to be created correctly\n"
+  read -s -n 1 -p "Press any key to exit"
+  exit 1
+fi
 
 #echo 'list created namespaces: '
 #su hbase -c 'echo "list_namespace" | hbase shell -n'
